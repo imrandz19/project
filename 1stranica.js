af7507d4583b688d656149b8f361058d39db98a9
Imran <imran.dzanovic2220c@size.ba>
2022-03-29 20:17:37 +0200

    i fixed origin and destination

--- 1stranica.js
+++ 1stranica.js
@@ -1,33 +1,14 @@
-function Radi(){
-    console.log(document.getElementById("from").value);
-};
-
-var mapProp={
-    center:new google.maps.LatLng(43.856430, 18.413029),
+var mapProp = {
+    center:new google.maps.LatLng({lat: 43.856430, lng: 18.413029}),
     zoom:11,
-};
-//   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
 
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 var directionsService = new google.maps.DirectionsService();
-var directionsRenderer = new google.maps.DirectionsRenderer();
+var directionsDisplay = new google.maps.DirectionsRenderer();
 
 directionsDisplay.setMap(map);
-
-// function myMap()
-// {    var mapProp={
-//         center:new google.maps.LatLng(43.856430, 18.413029),
-//         zoom:11,
-//     };
-//     //   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-//     var directionsService = new google.maps.DirectionsService();
-//     var directionsRenderer = new google.maps.DirectionsRenderer();
-
-//     directionsDisplay.setMap(map);}
 
 function calculateDistance(){
     var request = {
@@ -36,6 +17,7 @@ function calculateDistance(){
         travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.METRIC
     }
+    
     directionsService.route(request, function(result, status) {
         if (status === google.maps.DirectionsStatus.OK){
             const output = document.querySelector('#output');
@@ -44,79 +26,9 @@ function calculateDistance(){
         }
         else {
             directionsDisplay.setDirections({routes: []})
+            map.setCenter({lat: 43.856430, lng: 18.413029})
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i>Nemoguće izračunati></div>"
             
         }
     })
 }
-
-// function myMap(){
-//     var mapProp={
-//       center:new google.maps.LatLng(43.856430, 18.413029),
-//       zoom:11,
-//     };
-//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-// }
-
-
-// function initMap(){
-
-// const directionsRenderer = new google.maps.DirectionsRenderer();
-// const directionsService = new google.maps.DirectionsService();
-// const map = new google.maps.Map(document.getElementById("googleMap"), {
-//     zoom: 11,
-//     center: {lat:43.85, lng:18.41},
-// })
-
-
-// directionsRenderer.setMap(googleMap);
-// calculateAndDisplayRoute(directionsService, directionsRenderer);
-
-
-// function calculateAndDisplayRoute(directionsService, directionsRenderer){
-//     // const selectedMode = google.maps.TravelMode.DRIVING,
-
-//     directionsService
-//     .route({
-//         origin: document.getElementById("from").value,
-//         destination: document.getElementById("to").value,
-//         travelMode: google.maps.TravelMode.DRIVING,
-//         unitSystem: google.maps.UnitSystem.METRIC
-//     })
-//     .then((response) => {
-//         directionsRenderer.setDirections(response);
-//         console.log(response);
-//     })
-//     .catch((e) => window.alert("Direction request failed due to " + status));
-// }
-
-// function calcRoute(directionsService, directionsRenderer){
-//     var request ={
-//         origin: document.getElementById("from").value,
-//         destination: document.getElementById("to").value,
-//         travelMode: google.maps.TravelMode.DRIVING,
-//         unitSystem: google.maps.UnitSystem.METRIC
-//     }
-// }
-
-// directionsService.route(request, (result, status) =>{
-//     if (status==google.maps.DirectionsStatus.OK){
-//         const output=document.querySelector('#test');
-//         output.innerHTML ="<div class='alert-info'> From:" + document.getElementById("from").value + ".<br/>To:" + document.getElementById("to").value + ". <br/> Driving distance" + result.routes[0].legs[0].distance.text + ".<br/>Duration" + result.routes[0].legs[0].duration.text + ".</div>";
-    
-//         directionsDisplay.setDirections(result);
-//     }   else{
-//         directionsDisplay.setDirections({routes:[]});
-//         map.setCenter(myLatLng);
-//         output.innerHTML ="<div class='alert-danger'></i> Could not retrieve driving distance. </div>";
-//     }
-// });
-
-// var options ={
-//     types:['(cities)']
-// }
-
-// var input1 = document.getElementById("from");
-// var autocomplete1 = new google.maps.places.Autocomplete(input1, options)
-
-// var input2 = document.getElementById("to");
-// var autocomplete2 = new google.maps.places.Autocomplete(input2, options)
